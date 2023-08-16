68052fdaa4b13b060f10499c25ff1b0d93610302
Jr <jubayerrahman203@gmail.com>
2023-08-16 06:21:42 +0600

    state menegment

--- src/features/post/ui/PostForm.jsx
+++ src/features/post/ui/PostForm.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PostForm = () => {
+  const [Info, setInfo] = useState({ title: "", body: "" });
+
+  const hendelCng = (e) => {
+    const { name, value } = e.target;
+    if (value) {
+      setInfo((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
   return (
     <form>
-      <input type="text" placeholder="title" />
-      <textarea type="text" placeholder="body" />
+      <input
+        type="text"
+        placeholder="title"
+        name="title"
+        onChange={hendelCng}
+        value={Info.title}
+      />
+      <textarea
+        type="text"
+        placeholder="body"
+        name="body"
+        onChange={hendelCng}
+        value={Info.body}
+      />
       <button>post</button>
     </form>
   );
